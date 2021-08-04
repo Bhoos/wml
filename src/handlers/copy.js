6c8f2a51917029a61c556bd1565b79368a98b5ee
Ranjan Shrestha <ranjan@sharingapples.com>
2021-08-04 14:22:40 +0545

    watchman ignore_dirs didn't work on certain instances, so handling on wml side

--- src/handlers/copy.js
+++ src/handlers/copy.js
@@ -6,10 +6,23 @@ var fs = require('fs-extra');
 const { S_IRUSR, S_IRGRP, S_IROTH } = require('constants');
 
 module.exports = function (config) {
+	// Check for ignore_dirs and avoid copying ignored directory path
+	const configFile = path.join(config.src, '.watchmanconfig');
+	let ignoreDirs = null;
+	if (fs.existsSync(configFile)) {
+		const config = fs.readJSONSync(configFile);
+		ignoreDirs = config && config.ignore_dirs && config.ignore_dirs.map(k => k + path.sep);
+	}
+	
 	return function (resp) {
 		for (var i in resp.files) {
 			var f = resp.files[i];
 			if (f.type === 'f') {
+				if (ignoreDirs && ignoreDirs.find(dir => f.name.startsWith(dir))) {
+					console.log('[ignore]', f.name);
+					continue;
+				}
+
 				let src = path.join(config.src, f.name),
 				    dest = path.join(config.dest, f.name);
 
